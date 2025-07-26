38d1fa9914cf4dd363530c0789c4cdd03b8b43dc
Nina Menezes <nmenezes0@example.com>
2025-07-26 20:45:31 +0100

    format with ruff

--- home/models.py
+++ home/models.py
@@ -3,9 +3,8 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 
+
 class HomePage(Page):
     body = RichTextField(blank=True)
-    
+
     content_panels = Page.content_panels + ["body"]
-
-
