c951e335542ad564a4d767e4da15996fb20db922
Nina Menezes <nmenezes0@example.com>
2025-07-27 08:15:40 +0100

    get children

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-from wagtail.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
 
 from wagtail.search import index
@@ -8,6 +10,12 @@ from wagtail.search import index
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by("-first_published_at")
+        context["blogpages"] = blogpages
+        return context
 
     content_panels = Page.content_panels + ["intro"]
 
@@ -23,3 +31,14 @@ class BlogPage(Page):
     ]
 
     content_panels = Page.content_panels + ["date", "intro", "body"]
+
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(
+        BlogPage, on_delete=models.CASCADE, related_name="gallery_images"
+    )
+    image = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
+    )
+    caption = models.CharField(blank=True, max_length=250)
+    panels = ["image", "caption"]
